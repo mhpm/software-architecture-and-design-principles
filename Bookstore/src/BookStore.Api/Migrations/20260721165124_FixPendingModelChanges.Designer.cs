2280d17625ec7da0d48c4389b5bad6150c94a33a
SeniorDeloitte <seniordeloitte@gmail.com>
2026-07-21 11:09:18 -0600

    fix pending migration

--- Bookstore/src/BookStore.Api/Migrations/20260721165124_FixPendingModelChanges.Designer.cs
+++ Bookstore/src/BookStore.Api/Migrations/20260721165124_FixPendingModelChanges.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using BookStore.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace BookStore.Api.Migrations
 {
     [DbContext(typeof(BookStoreDbContext))]
-    partial class BookStoreDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260721165124_FixPendingModelChanges")]
+    partial class FixPendingModelChanges
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
